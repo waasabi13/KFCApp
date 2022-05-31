e8692c77897690e34629c60498c89ff60e20f83e
rmyau <91686883+rmyau@users.noreply.github.com>
2022-06-01 00:35:25 +0300

    Json dataBase

--- KFCFs/Drink.fs
+++ KFCFs/Drink.fs
@@ -1,6 +1,6 @@
 ﻿namespace KFC_Base
 
-type Burger(name: string, compound: string, kcal: double,proteins: double, fats: double, carbohydrates: double, price:double, pictureUrl: string, weight:double,size: Size, hot:bool ) = 
+type Drink(name: string, compound: string, kcal: double,proteins: double, fats: double, carbohydrates: double, price:double, pictureUrl: string, weight:double,size: Size, hot:bool ) = 
     inherit KFCItemsBase(name, compound, kcal,proteins, fats, carbohydrates, price, pictureUrl, weight)
     member this.Hot = hot
     member this.Size = size
---
+++ KFCFs/JsonDB.fs
@@ -0,0 +1,29 @@
+﻿namespace KFC_Base
+
+open System.IO
+open Newtonsoft.Json.Linq
+
+type JsonDB(file: string)=
+    let Items:(KFCItemsBase list)=
+        let rootObj = JObject.Parse(File.ReadAllText(file))
+        let burgers = rootObj.["BURGERS"].ToObject<Burger list>()
+        let chicken = rootObj.["Fried Chicken"].ToObject<FriedChicken list>()
+        let snacks = rootObj.["Snacks"].ToObject<Snack list>()
+        let drinks = rootObj.["Drinks"].ToObject<Drink list>()
+
+        List.map (fun b -> upcast b) burgers @ List.map  (fun c -> upcast c) chicken @ List.map  (fun s -> upcast s) snacks @ List.map  (fun d -> upcast d) drinks
+    
+    member this.Menu() = 
+        Items
+
+    member this.GetAllBurgers() = 
+        List.filter (fun (item: KFCItemsBase) -> item :? Burger) Items
+
+    member this.GetAllChicken() = 
+        List.filter (fun (item: KFCItemsBase) -> item :? FriedChicken) Items
+
+    member this.GetAllSnacks() = 
+        List.filter (fun (item: KFCItemsBase) -> item :? Snack) Items
+
+    member this.GetAllDrinks() = 
+        List.filter (fun (item: KFCItemsBase) -> item :? Drink) Items
