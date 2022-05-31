4d3dc12bedba2b908e9d10ad9496d2226392ee07
rmyau <91686883+rmyau@users.noreply.github.com>
2022-06-01 01:02:04 +0300

    Начало

---
+++ KFCFront/FrontForm.cs
@@ -0,0 +1,21 @@
+﻿using KFC_Base;
+using System.Windows.Forms;
+using Microsoft.FSharp.Collections;
+using System;
+using Microsoft.FSharp.Core;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+namespace KFCFront
+{
+    public partial class FrontForm : Form
+    {
+        private JsonDB db;
+        private FSharpList<KFCItemsBase> selectedItems;
+        //private Bitmap load
+        public FrontForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
