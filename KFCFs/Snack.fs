e18709df064d67a6beb5b61a5e2b7bd8247326d5
rmyau <91686883+rmyau@users.noreply.github.com>
2022-06-01 00:02:43 +0300

    Base structure

---
+++ KFCFs/Snack.fs
@@ -0,0 +1,5 @@
+﻿namespace KFC_Base
+
+type Burger(name: string, compound: string, kcal: double,proteins: double, fats: double, carbohydrates: double, price:double, pictureUrl: string, weight:double,size: Size ) = 
+    inherit KFCItemsBase(name, compound, kcal,proteins, fats, carbohydrates, price, pictureUrl, weight)
+    member this.Size = size
